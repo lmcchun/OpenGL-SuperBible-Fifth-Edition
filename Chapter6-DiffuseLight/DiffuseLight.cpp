2e6af109c3fe1b8dd3254ca6e98ac680b2fd1ffb
lmc_chun <lmc_chun@msn.com>
2020-03-21 20:08:32 +0800

    Chapter6-DiffuseLight.

---
+++ Chapter6-DiffuseLight/DiffuseLight.cpp
@@ -0,0 +1,121 @@
+#include <GLTools.h> // OpenGL toolkit
+#include <GLMatrixStack.h>
+#include <GLFrame.h>
+#include <GLFrustum.h>
+#include <GLGeometryTransform.h>
+#include <StopWatch.h>
+
+#include <math.h>
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+GLFrame             viewFrame;
+GLFrustum           viewFrustum;
+GLTriangleBatch     sphereBatch;
+GLMatrixStack       modelViewMatrix;
+GLMatrixStack       projectionMatrix;
+GLGeometryTransform transformPipeline;
+GLShaderManager     shaderManager;
+
+GLuint	diffuseLightShader;	// The diffuse light shader
+GLint	locColor;			// The location of the diffuse color
+GLint	locLight;			// The location of the Light in eye coordinates
+GLint	locMVP;				// The location of the ModelViewProjection matrix uniform
+GLint	locMV;				// The location of the ModelView matrix uniform
+GLint	locNM;				// The location of the Normal matrix uniform
+
+// 这个函数能够在渲染环境中进行任何重要的初始化
+void SetupRC(void)
+{
+	// 背景
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	shaderManager.InitializeStockShaders();
+	viewFrame.MoveForward(4.0f);
+	// 创建球体
+	gltMakeSphere(sphereBatch, 1.0f, 26, 13);
+	const char* szVertexProgFileName = "../Res/Chapter6-DiffuseLight/DiffuseLight.vp";
+	const char* szFragmentProgFileName = "../Res/Chapter6-DiffuseLight/DiffuseLight.fp";
+	diffuseLightShader = gltLoadShaderPairWithAttributes(szVertexProgFileName, szFragmentProgFileName, 2, GLT_ATTRIBUTE_VERTEX, "vVertex", GLT_ATTRIBUTE_NORMAL, "vNormal");
+	locColor = glGetUniformLocation(diffuseLightShader, "diffuseColor");
+	locLight = glGetUniformLocation(diffuseLightShader, "vLightPosition");
+	locMVP = glGetUniformLocation(diffuseLightShader, "mvpMatrix");
+	locMV = glGetUniformLocation(diffuseLightShader, "mvMatrix");
+	locNM = glGetUniformLocation(diffuseLightShader, "normalMatrix");
+}
+
+// Cleanup
+void ShutdownRC(void)
+{
+	//
+}
+
+// 进行调用以绘制场景
+void RenderScene(void)
+{
+	static CStopWatch rotTimer;
+	// 清除窗口和深度缓冲区
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	modelViewMatrix.PushMatrix(viewFrame);
+	modelViewMatrix.Rotate(rotTimer.GetElapsedSeconds() * 10.0f, 0.0f, 1.0f, 0.0f);
+	GLfloat vEyeLight[] = { -100.0f, 100.0f, 100.0f };
+	GLfloat vDiffuseColor[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+	glUseProgram(diffuseLightShader);
+	glUniform4fv(locColor, 1, vDiffuseColor);
+	glUniform3fv(locLight, 1, vEyeLight);
+	glUniformMatrix4fv(locMVP, 1, GL_FALSE, transformPipeline.GetModelViewProjectionMatrix());
+	glUniformMatrix4fv(locMV, 1, GL_FALSE, transformPipeline.GetModelViewMatrix());
+	glUniformMatrix3fv(locNM, 1, GL_FALSE, transformPipeline.GetNormalMatrix());
+	sphereBatch.Draw();
+	modelViewMatrix.PopMatrix();
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void ChangeSize(int w, int h)
+{
+	// Prevent a divide by zero
+	if (h == 0)
+	{
+		h = 1;
+	}
+
+	// Set Viewport to window dimensions
+	glViewport(0, 0, w, h);
+
+	viewFrustum.SetPerspective(35.0f, (float)w / (float)h, 1.0f, 100.0f);
+
+	projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
+	transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
+}
+
+// Main entry point for GLUT based programs
+int main(int argc, char* argv[])
+{
+	gltSetWorkingDirectory(argv[0]);
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Simple Diffuse Lighting");
+	glutReshapeFunc(ChangeSize);
+	glutDisplayFunc(RenderScene);
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+		return 1;
+	}
+
+	SetupRC();
+	glutMainLoop();
+	ShutdownRC();
+
+	return 0;
+}
